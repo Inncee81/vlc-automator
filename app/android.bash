8619ea33260a5b66e11c514ef860da04cad2497d
Whitey McCarragher <matt.mccarragher@online-rewards.com>
2018-08-16 14:15:48 -0400

    updated scripts

--- app/android.bash
+++ app/android.bash
@@ -11,7 +11,6 @@ linux_install(){
   # Download dependencies
   curl "https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip" -o sdk-tools-linux.zip
   curl "https://dl.google.com/android/repository/android-ndk-r14b-linux-x86_64.zip" -o android-ndk-r14b-linux.zip
-  
   unzip sdk-tools-linux.zip -d sdk/
   unzip android-ndk-r14b-linux.zip -d ndk/
 
@@ -32,18 +31,24 @@ macos_install(){
   # Download dependencies
   curl "https://dl.google.com/android/repository/sdk-tools-darwin-4333796.zip" -o sdk-tools-linux.zip
   curl "https://dl.google.com/android/repository/android-ndk-r14b-darwin-x86_64.zip" -o android-ndk-r14b-darwin.zip
-  
   unzip sdk-tools-darwin.zip -d sdk/
   unzip android-ndk-r14b-darwin.zip -d ndk/
   
-  # This still doesn't work right
-  brew install automake ant cmake gpatch subversion zlib llvm
+  # Should be all of the required libraries
+  brew install gettext automake autoconf ant cmake gtk+ gpatch subversion zlib llvm libtool
+  brew link --force gettext
+  link /usr/local/bin/glibtoolize /usr/local/bin/libtoolize
+
+  # Disables assembler as it causes errors
+  sed -i "contrib.src.ffmpeg.rules.mak" '28a\
+  --disable-asm ' vlc-android/vlc/contrib/src/ffmpeg/rules.mak
 }
 
 # Build both native and emulator versions
 build(){
   if [ `uname` == "Darwin" ] ; then
     # macOS!
+    alias libtoolize='glibtoolize'
     export JAVA_HOME='/Library/Java/Home/'
   else
     # Linux! (Ubuntu)
